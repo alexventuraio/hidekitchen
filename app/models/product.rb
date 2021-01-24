2c5547f5d2cba7571c90f952ec28c018403180fe
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-01-24 13:54:38 -0600

    Add some model validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :restaurant
+  validates :name, :price, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :products
+
+  validates :name, :domain, presence: true
 end
